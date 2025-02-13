4432812e7681215ae12cd1e3ecda4159fbfdc653
JeremyJamesL <jeremyluscombe@gmail.com>
2025-02-13 22:03:34 +0000

    copyMarkdown update

--- scripts/copyMarkdown.js
+++ scripts/copyMarkdown.js
@@ -1,4 +1,4 @@
-const nunFileString = `---\nlayout: /layouts/layout.njk\ntitle: All notes\ndescription: A list of all notes\n---\n{% set coursesArr = [] %}\n\n{% for item in collections.coursesList %}\n\t{% if coursesArr.indexOf(item.data.course) == -1 %}\n\t\t{% set coursesArr = (coursesArr.push( item.data.course ), coursesArr) %}\n\t{% endif %}\n{% endfor %}\n\n<div class=\"container\">\n\n<ul class=\"list-disc pl-3 font-body font-light\">\n{% for theCourse in coursesArr %}\n\t<li class=\"mb-3\">\n\t\t<h2 class=\"mb-2\">{{ theCourse }}</h2>\n\n\t\t{% for item in collections.coursesList %}\n\t\t\t{% if item.data.course == theCourse %}\n\t\t\t<h3>\n\t\t\t\t<a href=\"{{ item.url }}\" class=\"text-[18px] mr-2 visited:text-red-600 no-underline\">\n\t\t\t\t\t{{ item.data.title }}\n\t\t\t\t</a>\n\t\t\t</h3>  \n\t\t\t{% endif %}\n\t\t{% endfor %}\n\t</li>\n{% endfor %}\n</ul>\n\n</div> \n`
+
 
 const fs = require("fs");
 const path = require("path");
@@ -7,6 +7,9 @@ const outputFolderName = "/Users/jeremy.luscombe/websites/jezl-11ty/notes/";
 const dirs = fs.readdirSync(folderName);
 
 // Wipe /notes and all its contents
+
+const njkFileData = fs.readFileSync(outputFolderName + "index.njk");
+
 
 fs.rm(outputFolderName, { recursive: true, force: true }, (err) => {
   if (err) {
@@ -18,7 +21,7 @@ fs.rm(outputFolderName, { recursive: true, force: true }, (err) => {
       console.log(err);
     }
 
-    fs.writeFileSync(outputFolderName + "index.njk", nunFileString);
+    fs.writeFileSync(outputFolderName + "index.njk", njkFileData);
 
     dirs.forEach((dir) => {
       const stats = fs.statSync(folderName + dir);
